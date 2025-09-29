873130b5e35ceeedfaa5e1d3e049b2f844318aad
Terrablood <toth.levente.22f@szlgbp.hu>
2025-09-29 17:50:26 +0200

    modellek_vannak

--- app_hatos/models.py
+++ app_hatos/models.py
@@ -2,17 +2,49 @@ from django.db import models
 
 class Huzas(models.Model):
 
-    id = models.models.IntegerField(_("256"))
-    ev = models.models.IntegerField(_("256"))
-    het = models.models.IntegerField(_("256"))
+    idd = models.IntegerField()
+    ev = models.IntegerField()
+    het = models.IntegerField()
 
     class Meta:
-        verbose_name = _("huzas")
-        verbose_name_plural = _("huzasok")
+        verbose_name = "huzas"
+        verbose_name_plural = "huzasok"
 
     def __str__(self):
-        return f'{self.id} ({self.ev}.{self.het})'
+        return f'{self.idd} ({self.ev}.{self.het})'
 
+class Huzott(models.Model):
+
+    idd = models.IntegerField()
+    huzasid = models.IntegerField()
+    szam = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Huzott"
+        verbose_name_plural = "Huzottak"
+
+    def __str__(self):
+        return f'{self.idd} ({self.huzasid}.{self.szam})'
+
+    
+
+class Nyeremeny(models.Model):
+
+    idd = models.IntegerField()
+    huzasid = models.IntegerField()
+    talalat = models.IntegerField()
+    darab = models.IntegerField()
+    ertek = models.IntegerField()
+    
+
+    class Meta:
+        verbose_name = "Nyeremeny"
+        verbose_name_plural = "Nyeremenyek"
+
+    def __str__(self):
+        return f'{self.idd} ({self.huzasid}.{self.talalat}.{self.darab}.{self.ertek})'
+
+    
 
     
 
