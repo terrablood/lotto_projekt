7765427b3a514b4a60f2dd3613c52d28e4c8e427
Terrablood <toth.levente.22f@szlgbp.hu>
2025-10-11 18:42:43 +0200

    elso_megvan

--- app_hatos/views.py
+++ app_hatos/views.py
@@ -8,12 +8,29 @@ def kerdesek(request: HttpRequest):
     context = {}
     return render(request, template, context)
 
-  
-
+# 1 
+#Készítsen lekérdezést, amely megadja, hogy az előző évezredben (2001. január 1. előtt)
+#mikor volt hat találatos szelvény! A húzáshoz tartozó évet és hetet jelenítse meg időrendben!
 def elso(request: HttpRequest):
     template = 'app_hatos/elso.html'
-    context = {}
+    
+    huzasok = Huzas.objects.all
+    nyermenyek = Nyeremeny.objects.all
+    hatos_talalatok = []
+
+    for nyeremeny in nyermenyek:
+        if(nyeremeny.talalat == 6):
+            for huzas in huzasok:
+                if(huzas.ev >= 2001, huzas.idd == nyeremeny.huzasid):
+                    hatos_talalatok.append(huzas.ev)
+
+    context = {
+        'hatosok_2001elott' : hatos_talalatok,
+    }
     return render(request, template, context)
+
+
+
 def masodik(request: HttpRequest):
     template = 'app_hatos/masodik.html'
     context = {}
